3cd5e7299003cf552fbfded3889292b2afa0fd61
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-09-12 12:11:15 +0530

    PDX-352: base class for generic output printing
    RCA: was addingoptional parameters to generic functions for every command
    
    Fix: instead created generic class that can be extended for every command having diff output types

---
+++ src/Utility/SfProvarResult.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 Provar Limited.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.md file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { Messages } from '@salesforce/core';
+import { ErrorHandler, Error } from './errorHandler.js';
+import { GenericErrorHandler } from './genericErrorHandler.js';
+import { SfProvarCommandResult } from './sfProvarCommandResult.js';
+
+/* eslint-disable */
+export class SFProvarResult {
+  /**
+   * Declaring return type and populating return object for async run method of the commands.
+   *
+   */
+
+  public populateResult(
+    flags: any,
+    errorHandler: ErrorHandler | GenericErrorHandler,
+    messages: Messages<string>,
+    log: Function,
+    value?: string,
+    testCases?: any[]
+  ): SfProvarCommandResult {
+    let result: SfProvarCommandResult = { success: true };
+
+    const errorObjects: Error[] | object[] = errorHandler.getErrors();
+    if (errorObjects.length > 0) {
+      if (!flags['json']) {
+        throw messages.createError('error.MultipleFailure', errorHandler.errorsToStringArray());
+      }
+      result = {
+        success: false,
+        errors: errorObjects,
+      };
+    } else {
+      messages.messages.has('success_message') ? log(messages.getMessage('success_message')) : '';
+      value != null ? log(value) : '';
+      result = {
+        success: true,
+        value: value,
+      };
+      testCases != null && testCases.length > 0
+        ? (result = {
+            success: true,
+            testCases: testCases,
+          })
+        : [];
+    }
+
+    return result;
+  }
+}
